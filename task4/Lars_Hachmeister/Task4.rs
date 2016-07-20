621213c8b031a7b2207d444816a9bb11fcbea721
LarsHachmeister <lhachmeister@uni-osnabrueck.de>
2016-07-20 11:15:37 +0200

    add "swagger" to Task4.rs

--- task4/Lars_Hachmeister/Task4.rs
+++ task4/Lars_Hachmeister/Task4.rs
@@ -4,8 +4,8 @@ use std::marker::Copy;
 
 fn main() {
     println!("{:?}", summe(7, 5));
-    // let b = swagger { word: "test" };
-    // print!("{}", b);
+    let b = swagger { word: "test" };
+    println!("{}", b);
 }
 
 
@@ -13,12 +13,12 @@ fn summe<T: Add + Mul + Copy>(a: T, b: T) -> (<T as Add>::Output, <T as Mul>::Ou
     ((a + b), (a * b))
 }
 
-// struct swagger {
-//     word: T,
-// }
+struct swagger<T> {
+    word: T,
+}
 
-// impl Display for swagger {
-//     fn fmt(&self, f: &mut Formatter) -> Result {
-//         write!(f, "#swag {} #yolo)", self.word)
-//     }
-// }
+impl<T: Display> Display for swagger<T> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "#swag {} #yolo", self.word)
+    }
+}
