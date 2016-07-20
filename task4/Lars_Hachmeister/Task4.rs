94fd5d15b48cc99e66a2572ed5a00a28b24e43ef
LarsHachmeister <lhachmeister@uni-osnabrueck.de>
2016-07-20 12:09:25 +0200

    fix mistakes in Task4.rs and add Fibonaci-iterator to it

--- task4/Lars_Hachmeister/Task4.rs
+++ task4/Lars_Hachmeister/Task4.rs
@@ -1,11 +1,15 @@
 use std::ops::*;
 use std::fmt::*;
-use std::marker::Copy;
+use std::iter::Iterator;
 
 fn main() {
     println!("{:?}", summe(7, 5));
-    let b = swagger { word: "test" };
+    let b = Swagger { word: "test" };
     println!("{}", b);
+    for i in Counter::new().take(20) {
+        println!("{}", i);
+    }
+
 }
 
 
@@ -13,12 +17,38 @@ fn summe<T: Add + Mul + Copy>(a: T, b: T) -> (<T as Add>::Output, <T as Mul>::Ou
     ((a + b), (a * b))
 }
 
-struct swagger<T> {
+struct Swagger<T> {
     word: T,
 }
 
-impl<T: Display> Display for swagger<T> {
+
+
+impl<T: Display> Display for Swagger<T> {
     fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "#swag {} #yolo", self.word)
+    }
+}
+
+struct Counter {
+    current: u32,
+    previous: u32,
+}
+
+impl Counter {
+    pub fn new() -> Counter {
+        Counter {
+            current: 1,
+            previous: 0,
+        }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.previous;
+        self.previous = self.current;
+        self.current = self.current + self.previous;
+        Some(res)
     }
 }
